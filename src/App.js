c021d5d5a0c3cefc02d5b1af92ed3fc2601eeac6
Renato Moschetta <renatorock3@hotmail.com>
2020-11-23 12:13:23 -0300

    up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect } from "react";
+import io from "socket.io-client";
 
-function App() {
+const App = () => {
+  const socket = io.connect("http://localhost:3333");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      author: "Renato",
+      message: "Olá mundo",
+    };
+
+    socket.emit("send", data);
+  };
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log(socket.id);
+      socket.on("received", (data) => {
+        console.log(data);
+      });
+    });
+  }, [socket]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form id="chat">
+        <select id="username">
+          <option value="renato">Renato</option>
+          <option value="adriana">Adriana</option>
+        </select>
+        <div className="messages"></div>
+        <input type="text" name="message" placeholder="Digite sua mensagem" />
+        <button onClick={(e) => handleSubmit(e)}>Enviar</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
